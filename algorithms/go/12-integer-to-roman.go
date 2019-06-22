5274dd428c2e3ca3433c2710b8178b33e52f04d6
haokw <atwanghao@gmail.com>
2019-06-22 23:57:15 +0800

    12. Integer to Roman

---
+++ algorithms/go/12-integer-to-roman.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// strs := []string{"flower", "flow", "flight"}
+	// strs := []string{"dog", "racecar", "car"}
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 58, 54, 1994}
+	// nums := []int{9}
+	for _, num := range nums {
+		ret := intToRoman(num)
+		fmt.Println(ret)
+	}
+}
+
+func intToRoman(num int) string {
+	symbols := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+	values := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	ret := ""
+
+	if num < 1 || num > 3999 {
+		return ret
+	}
+
+	roman := ""
+	for i := 0; num > 0; i++ {
+		for num >= values[i] {
+			num -= values[i]
+			roman += symbols[i]
+		}
+	}
+
+	ret = roman
+	return ret
+}
