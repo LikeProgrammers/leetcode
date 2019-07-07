6ca3b97b089b8c1c44627a79172b5d1008bcc6b2
haokw <atwanghao@gmail.com>
2019-07-07 12:13:35 +0800

    100. Same Tree

---
+++ algorithms/go/100-same-tree/100-same-tree.go
@@ -0,0 +1,30 @@
+package isSameTree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	// p and q are both nil
+	if p == nil && q == nil {
+		return true
+	}
+	// one of p and q is nil
+	if q == nil || p == nil {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
