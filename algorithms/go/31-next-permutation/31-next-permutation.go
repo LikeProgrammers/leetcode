321344a27d3e6768ada77e2dda6db555c0de005a
haokw <atwanghao@gmail.com>
2019-08-01 21:51:47 +0800

    fix indent

--- algorithms/go/31-next-permutation/31-next-permutation.go
+++ algorithms/go/31-next-permutation/31-next-permutation.go
@@ -2,16 +2,16 @@ package nextPermutation
 
 import "sort"
 
-func nextPermutation(nums []int)  {
-    if len(nums) < 2 {
-        return
-    }
-    for i := len(nums) - 1; i > 0; i-- {
-        if nums[i] > nums[i-1] {
-            nums[i], nums[i-1] = nums[i-1], nums[i]
-            return
-        }
-    }
-    sort.Ints(nums)
-    return
+func nextPermutation(nums []int) {
+	if len(nums) < 2 {
+		return
+	}
+	for i := len(nums) - 1; i > 0; i-- {
+		if nums[i] > nums[i-1] {
+			nums[i], nums[i-1] = nums[i-1], nums[i]
+			return
+		}
+	}
+	sort.Ints(nums)
+	return
 }
