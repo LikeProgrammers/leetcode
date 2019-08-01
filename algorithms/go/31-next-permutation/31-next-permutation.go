970953db203c3a8eefd96f5cc744c3094157f3a6
haokw <atwanghao@gmail.com>
2019-08-01 16:59:25 +0800

    31. Next Permutation

---
+++ algorithms/go/31-next-permutation/31-next-permutation.go
@@ -0,0 +1,17 @@
+package nextPermutation
+
+import "sort"
+
+func nextPermutation(nums []int)  {
+    if len(nums) < 2 {
+        return
+    }
+    for i := len(nums) - 1; i > 0; i-- {
+        if nums[i] > nums[i-1] {
+            nums[i], nums[i-1] = nums[i-1], nums[i]
+            return
+        }
+    }
+    sort.Ints(nums)
+    return
+}
