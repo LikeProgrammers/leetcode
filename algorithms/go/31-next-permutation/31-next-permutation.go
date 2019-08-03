44c91320356cbfc3b67fa36d8b774d00734febcf
haokw <atwanghao@gmail.com>
2019-08-03 09:29:46 +0800

    update indent

--- algorithms/go/31-next-permutation/31-next-permutation.go
+++ algorithms/go/31-next-permutation/31-next-permutation.go
@@ -1,30 +1,30 @@
 package nextPermutation
 
 func nextPermutation(nums []int) {
-    i := len(nums) - 2
-    for i >= 0 && nums[i] >= nums[i+1] {
-        i--
-    }
-    if i >= 0 {
-        j := len(nums) - 1
-        for j >= 0 && nums[j] <= nums[i] {
-            j--
-        }
-        swap(nums, i, j)
-    }
-    reverse(nums, i + 1)
+	i := len(nums) - 2
+	for i >= 0 && nums[i] >= nums[i+1] {
+		i--
+	}
+	if i >= 0 {
+		j := len(nums) - 1
+		for j >= 0 && nums[j] <= nums[i] {
+			j--
+		}
+		swap(nums, i, j)
+	}
+	reverse(nums, i+1)
 }
 
 func reverse(nums []int, start int) {
-    i := start
-    j := len(nums) - 1
-    for i < j {
-        swap(nums, i, j)
-        i++
-        j--
-    }
+	i := start
+	j := len(nums) - 1
+	for i < j {
+		swap(nums, i, j)
+		i++
+		j--
+	}
 }
 
 func swap(nums []int, i int, j int) {
-    nums[i], nums[j] = nums[j], nums[i]
+	nums[i], nums[j] = nums[j], nums[i]
 }
