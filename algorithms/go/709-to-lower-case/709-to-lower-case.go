08c39c3f04da2ac61824e92f3461fb789ab4d42c
haokw <atwanghao@gmail.com>
2019-08-08 15:26:34 +0800

    709. To Lower Case

---
+++ algorithms/go/709-to-lower-case/709-to-lower-case.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main () {
+	str := "Hello"
+	ret := toLowerCase(str);
+	fmt.Println(ret)
+}
+
+func toLowerCase(str string) string {
+	return strings.ToLower(str)
+}
